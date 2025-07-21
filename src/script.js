ec3d37da4fa69e92e517bf4c73c3bd3b2a6781de
Boofa Legion Linux <106235648+RyleyBoofa@users.noreply.github.com>
2025-07-21 17:53:24 +1000

    feat(webpage): implement basic webpage design
    Updated starter files to reflect primitive version of project design

--- src/script.js
+++ src/script.js
@@ -1,1 +1,17 @@
-console.log("Hello, World");
+const GRID_SIZE = 16;
+
+const gridContainer = document.querySelector(".grid-container");
+const itemWidth = gridContainer.offsetWidth / GRID_SIZE;
+const itemHeight = gridContainer.offsetHeight / GRID_SIZE;
+console.log(itemWidth);
+
+for (let i = 0; i < GRID_SIZE; i++) {
+    for (let j = 0; j < GRID_SIZE; j++) {
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item");
+        gridItem.id = `row${i}-col${j}`;
+        gridItem.style.width = `${itemWidth}px`;
+        gridItem.style.height = `${itemHeight}px`;
+        gridContainer.appendChild(gridItem);
+    }
+}

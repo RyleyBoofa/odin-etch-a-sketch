23b0f3863fbaec92b60d84c1b6305d8e263ac6d1
Boofa Legion Linux <106235648+RyleyBoofa@users.noreply.github.com>
2025-07-22 11:01:58 +1000

    feat(webpage): complete basic layout and logic
    Completed basic page layout including grid container and resize button.
    User is able to "draw" to the grid using their mouse and can resize the
    grid using the provided button.

--- src/script.js
+++ src/script.js
@@ -1,17 +1,62 @@
-const GRID_SIZE = 16;
-
+const DEFAULT_GRID_SIZE = 16;
 const gridContainer = document.querySelector(".grid-container");
-const itemWidth = gridContainer.offsetWidth / GRID_SIZE;
-const itemHeight = gridContainer.offsetHeight / GRID_SIZE;
-console.log(itemWidth);
+document.querySelector("#grid-button").addEventListener("click", resizeGrid);
 
-for (let i = 0; i < GRID_SIZE; i++) {
-    for (let j = 0; j < GRID_SIZE; j++) {
-        const gridItem = document.createElement("div");
-        gridItem.classList.add("grid-item");
-        gridItem.id = `row${i}-col${j}`;
-        gridItem.style.width = `${itemWidth}px`;
-        gridItem.style.height = `${itemHeight}px`;
-        gridContainer.appendChild(gridItem);
+function createGrid(size = DEFAULT_GRID_SIZE) {
+    addGridItems(size);
+    addGridEventListener();
+}
+
+function resizeGrid() {
+    const size = getNewGridSize();
+    clearGrid();
+    createGrid(size);
+}
+
+function addGridItems(size) {
+    const itemWidth = gridContainer.offsetWidth / size;
+    const itemHeight = gridContainer.offsetHeight / size;
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const gridItem = document.createElement("div");
+            gridItem.classList.add("grid-item");
+            gridItem.id = `row${i}-col${j}`;
+            gridItem.style.width = `${itemWidth}px`;
+            gridItem.style.height = `${itemHeight}px`;
+            gridContainer.appendChild(gridItem);
+        }
     }
 }
+
+function addGridEventListener() {
+    gridContainer.addEventListener(
+        "mouseenter",
+        (e) => {
+            e.target.style.backgroundColor = "black";
+        },
+        true
+    );
+}
+
+function getNewGridSize() {
+    let newSize = 0;
+    while (newSize < 1) {
+        let input = parseInt(prompt("Input new grid size"));
+        if (Number.isInteger(input)) {
+            if (input > 0 && input <= 100) {
+                newSize = input;
+            }
+        }
+    }
+    return newSize;
+}
+
+function clearGrid() {
+    Array.from(gridContainer.children).forEach((gridItem) => {
+        console.log(gridItem);
+        gridItem.remove();
+    });
+}
+
+createGrid();
